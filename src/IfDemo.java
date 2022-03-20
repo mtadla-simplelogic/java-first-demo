b90d11f829251d46d6efc4ea83f08bc77b817b44
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-03-20 09:36:19 +0100

    boolean and char

---
+++ src/IfDemo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        int x = scanner.nextInt();
+        System.out.println(x);
+    }
+}
