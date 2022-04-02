7f8d807104474d38632c5d60fbef6935ebb40923
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-04-02 12:34:07 +0200

    salary and array demo

--- src/arrays/Salary.java
+++ src/arrays/Salary.java
@@ -2,27 +2,27 @@ package arrays;
 
 public class Salary {
     public static void main(String[] args) {
-        int[] tab = {10, 20, 333, 20, 50, 100, 123, 232, 15, 9};
+        int[] salaries = {10, 20, 333, 20, 50, 100, 123, 232, 15, 9};
 
         int sum = 0;
-        int max = tab[0];
-        int min = tab[0];
+        int max = salaries[0];
+        int min = salaries[0];
 
-        for (int i = 0; i < tab.length; i++) {
-            sum += tab[i];
+        for (int i = 0; i < salaries.length; i++) {
+            sum += salaries[i];
 
-            if (tab[i] > max) {
-                max = tab[i];
+            if (salaries[i] > max) {
+                max = salaries[i];
             }
 
-            if (tab[i] < min) {
-                min = tab[i];
+            if (salaries[i] < min) {
+                min = salaries[i];
             }
         }
 
         System.out.println("Max " + max);
         System.out.println("Min " + min);
         System.out.println("Suma " + sum);
-        System.out.println("Srednia " + (sum / tab.length));
+        System.out.println("Srednia " + (sum / salaries.length));
     }
 }
