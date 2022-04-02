be97a42172006431e21a8b63a012e129fea0214a
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-04-02 09:00:46 +0200

    refactor

--- src/loops/Square.java
+++ src/loops/Square.java
@@ -4,6 +4,7 @@ public class Square {
     public static void main(String[] args) {
         int size = 10;
 
+        
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
                 if (i == 1 || i == size || j == 1 || j == size) {
