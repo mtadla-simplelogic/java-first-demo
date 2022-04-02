6665dae8b63ef58ba26080551748c6000b2a4094
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-04-02 13:44:43 +0200

    salary and array demo

---
+++ src/arrays/Square.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class Square {
+    //14-15.05.2022
+    //28-29.05.2022
+    //11-12.06 2022
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[] tab = new int[n];
+    }
+//    Stwórz zmienna int n i przypisz to niej dowolną wartość >0.
+//    Stwórz tablice n elementową.
+//    Przy pomocy pętli ustaw wartości przechowywane w tablicy ->
+//    do każdego elementu przypisz kwadrat jego indeksu.
+//    Czyli np. tab[3] powinno przechowywać 9
+
+//    Wydrukuj tablicę do konsoli sformatowane jako:
+//    element o indeksie 1 ma wartość: 1
+//    element o indeksie 2 ma wartość: 4
+
+}
