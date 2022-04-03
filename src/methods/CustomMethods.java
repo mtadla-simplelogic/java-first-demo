56f74e151c0ee1a7cc8d1e0d310e9ce90379f940
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-04-03 10:27:18 +0200

    salary and array demo

--- src/methods/CustomMethods.java
+++ src/methods/CustomMethods.java
@@ -1,6 +1,21 @@
 package methods;
 
 public class CustomMethods {
+    public static void main(String[] args) {
+        printHello();
+        printBye();
+    }
+
+    public static void printHello() {
+        System.out.println("Hello");
+        printBye();
+    }
+
+    public static void printBye() {
+        System.out.println("Bye");
+    }
+
+
     // stworz 2 metody (printHello, printBye)
     // każda z nich powinna drukować swoja nazwę do konsoli
     // np. System.out.println("Moja nazwa to xxxxxxx")
@@ -8,8 +23,4 @@ public class CustomMethods {
     //
     // uruchom te dwie metody wewnątrz main
     // wewnatrz printHello uruchom printBye
-    
-    public static void main(String[] args) {
-
-    }
 }
