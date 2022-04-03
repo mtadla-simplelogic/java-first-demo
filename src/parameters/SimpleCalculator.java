32412365f3ba5fac34b49a964d2033e3871416d8
Mateusz Tadla <mtadla-simplelogic@gmail.com>
2022-04-03 15:28:19 +0200

    updated calc

--- src/parameters/SimpleCalculator.java
+++ src/parameters/SimpleCalculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SimpleCalculator {
     public static void main(String[] args) {
         printMenu();
+        
         int selectedOption = getNumberFromUser();
         int a = getNumberFromUser();
         int b = getNumberFromUser();
