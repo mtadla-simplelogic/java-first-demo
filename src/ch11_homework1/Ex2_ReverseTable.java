57ecea9e1eabd96cea3eb96c0684b7f419ba66a6
Mateusz Tadla <mat@scalepoint.com>
2022-04-23 09:55:32 +0200

    reverse tabel

--- src/ch11_homework1/Ex2_ReverseTable.java
+++ src/ch11_homework1/Ex2_ReverseTable.java
@@ -1,9 +1,14 @@
 package ch11_homework1;
 
+import java.util.Scanner;
+
 public class Ex2_ReverseTable {
     public static void main(String[] args) {
-        int[] numbers = {4, 5, 6, 7, 8, 9, 12, 34, 54, 23};
-        int[] reversed = new int[10];
+        System.out.println("please give me length size");
+        int length = getNumberFromUser();
+
+        int[] numbers = getArrayFromUser(length);
+        int[] reversed = new int[length];
 
         for (int i = 1; i <= numbers.length; i++) {
             reversed[i - 1] = numbers[numbers.length - i];
@@ -13,5 +18,19 @@ public class Ex2_ReverseTable {
             System.out.println(number);
         }
 
+    }
+
+    public static int[] getArrayFromUser(int length) {
+        int[] array = new int[length];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("please give me number " + (i + 1));
+            array[i] = getNumberFromUser();
+        }
+        return array;
+    }
+
+    public static int getNumberFromUser() {
+        return new Scanner(System.in).nextInt();
     }
 }
