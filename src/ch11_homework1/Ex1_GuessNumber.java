a2a4254d731bd7bd0b9ece466fe6bfce2b10546d
Mateusz Tadla <mat@scalepoint.com>
2022-04-23 09:31:31 +0200

    refactor

---
+++ src/ch11_homework1/Ex1_GuessNumber.java
@@ -0,0 +1,38 @@
+package ch11_homework1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex1_GuessNumber {
+    public static void main(String[] args) {
+        int numberToGuess = new Random().nextInt(10);
+        int maxNumberOfTries = 5;
+
+        for (int i = 1; i <= maxNumberOfTries; i++) {
+            int numberFromUser = getNumberFromUser();
+
+            if (numberFromUser == numberToGuess) {
+                System.out.println("you won!");
+                break;
+            }
+
+            if (numberFromUser > numberToGuess) {
+                System.out.println("Too big number");
+            } else {
+                System.out.println("Too low number");
+            }
+
+            if (i == maxNumberOfTries) {
+                System.out.println("you lost, aswear was: " + numberToGuess);
+            } else {
+                System.out.println("You have " + (maxNumberOfTries - i) + " tries left");
+            }
+
+        }
+    }
+
+    public static int getNumberFromUser() {
+        System.out.println("Prosze podaj liczbę");
+        return new Scanner(System.in).nextInt();
+    }
+}
