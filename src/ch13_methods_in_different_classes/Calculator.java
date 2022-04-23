a199798ae14b9f4bf05399a3a732769f9bf4d4b7
Mateusz Tadla <mat@scalepoint.com>
2022-04-23 11:06:08 +0200

    calculatr

---
+++ src/ch13_methods_in_different_classes/Calculator.java
@@ -0,0 +1,7 @@
+package ch13_methods_in_different_classes;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/ch13_methods_in_different_classes/Main.java
@@ -0,0 +1,7 @@
+package ch13_methods_in_different_classes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(4, 3));
+    }
+}
